3161bdd7f436863429678f3b7171133da78ae513
ranz <edward.francis.ibera@student.ateneo.edu>
2026-03-03 00:22:54 +0800

    Updated models.py

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,8 +1,22 @@
 from django.db import models
 from django.urls import reverse
+from django.core.validators import MinLengthValidator
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    bio = models.TextField(
+        validators=[
+            MinLengthValidator(255, "Bio must be atleast 255 characters long.")
+        ]
+    )
+
+    def __str__ (self):
+        return self.name
 
 class Ingredient(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.name
@@ -12,6 +26,9 @@ class Ingredient(models.Model):
 
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="recipe", null=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name 
@@ -33,4 +50,4 @@ class RecipeIngredient(models.Model):
     )
 
     def __str__(self):
-        return f'{self.quantity} {self.ingredient.name}'
+        return f"{self.quantity} {self.ingredient.name} in {self.recipe.name}"
